447182f1b73d852e344df14091946d638685e11c
Caprice NDOMBI MOUIRI <caprice-heridya.ndombi-mouiri@laposte.fr>
2024-08-06 17:53:53 +0200

    ajout de primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MAGE_LIST } from './mage-list.fake';
 import { Mage } from './mage.model';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ImageModule } from 'primeng/image';
+import { ToolbarModule } from 'primeng/toolbar';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, CardModule, ImageModule, ToolbarModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Equipe du Taureau Noir';
-  
+
   mageList = signal(MAGE_LIST);
 
   incrementLife(mage: Mage) {
@@ -19,14 +24,13 @@ export class AppComponent {
   }
 
   decrementLife(mage: Mage) {
-   mage.life = mage.life - 3;
+    mage.life = mage.life - 3;
   }
 
-  size(mage: Mage){
-    if(mage.life <= 10){
+  size(mage: Mage) {
+    if (mage.life <= 10) {
       return 'Enfant';
-    }
-    else if (mage.life >= 25){
+    } else if (mage.life >= 25) {
       return 'Adulte';
     }
 
